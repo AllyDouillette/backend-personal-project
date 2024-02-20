65f27f049f7378fc03e609cff633e0fe38540910
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-20 15:01:17 +0100

    add class for Card

--- src/helper/constructors.js
+++ src/helper/constructors.js
@@ -1,6 +1,18 @@
-export default class User {
+export class User {
   constructor (email, password) {
     this.email = email
     this.password = password
+  }
+}
+
+export class Card {
+  constructor (prompt, answer, hint = '') {
+    this.prompt = prompt
+    this.answer = answer
+    this.hint = hint
+  }
+
+  setCategory (id) {
+    this.categoryId = id
   }
 }
