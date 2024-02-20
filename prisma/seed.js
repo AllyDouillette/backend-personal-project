28bbc758cfe28cefa3be4862e9b0bb5ececdd119
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-20 16:37:46 +0100

    fix possible eventemitter memory leak

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,32 +1,34 @@
-import { PrismaClient } from '@prisma/client'
-import { User, Card, Category } from '../src/helper/constructors.js'
+import { PrismaClient } from "@prisma/client"
+import { User, Card, Category } from "../src/helper/constructors.js"
 const prisma = new PrismaClient()
 
 async function seed () {
-  const newUser = new User('Test@Something.com', 'password123')
-  const user = await prisma.user.create({
-    data: newUser
-  })
-  console.log(user)
+	const newUser = new User("Test@Something.com", "password123")
+	const user = await prisma.user.create({
+		data: newUser
+	})
+	console.log(user)
 
-  const newCategory = new Category('Test')
-  newCategory.setOwner(user.id)
-  const category = await prisma.category.create({
-    data: newCategory
-  })
-  console.log(category)
+	const newCategory = new Category("Test")
+	newCategory.setOwner(user.id)
+	const category = await prisma.category.create({
+		data: newCategory
+	})
+	console.log(category)
 
-  const newCard = new Card('Marco', 'Polo', 'no hint')
-  newCard.setOwner(user.id)
-  newCard.setCategory(category.id)
-  const card = await prisma.card.create({
-    data: newCard
-  })
-  console.log(card)
+	const newCard = new Card("Marco", "Polo", "no hint")
+	newCard.setOwner(user.id)
+	newCard.setCategory(category.id)
+	const card = await prisma.card.create({
+		data: newCard
+	})
+	console.log(card)
+
+	process.exit(0)
 }
 
 seed().catch(async (e) => {
-  console.error(e)
-  await prisma.$disconnect()
-  process.exit(1)
+	console.error(e)
+	await prisma.$disconnect()
+	process.exit(1)
 })
