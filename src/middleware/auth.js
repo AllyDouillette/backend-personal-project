87649f10a796afb606c62f502ec6a8c3bdacc9fa
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-20 18:38:35 +0100

    add rough draft of checkToken

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+import { constructMessageResponse } from "../helper/response"
+import { verifyToken } from "../helper/authentication.js"
+
+export const checkToken = async (req, res, next) => {
+	const authentication = req.header("authorization")
+	if (!authentication) {
+		return constructMessageResponse(res, 400, "missing authorization in header")
+	}
+
+	const [_, token] = authentication.split(" ")
+	if (!token) {
+		return constructMessageResponse(res, 400, "missing authorization in header")
+	}
+
+	if (verifyToken(token)) {
+		next()
+	}
+
+	return constructMessageResponse(res, 500)
+}
