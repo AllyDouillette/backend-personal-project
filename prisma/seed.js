ae1ef58cb0af608cce6a8cbcd2676f2529432a67
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-20 14:12:13 +0100

    set up seed file

---
+++ prisma/seed.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+
+async function seed() {
+  
+}
+
+seed().catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
