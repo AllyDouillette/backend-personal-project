9bac4abf889c0c17b4062ebd0ad4f0f06ae7b941
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-02-28 12:40:27 +0100

    fix wrong parameter name

--- src/domain/category.js
+++ src/domain/category.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 export const getCategoriesDb = async (includeCardDetails) => {
 	return await prisma.category.findMany({
 		include: {
-			Card: includeCardDetails
+			Cards: includeCardDetails
 		}
 	})
 }
@@ -15,7 +15,7 @@ export const getCategoriesFromOwnerDb = async (ownerId, includeCardDetails) => {
 			ownerId
 		},
 		include: {
-			Card: includeCardDetails
+			Cards: includeCardDetails
 		}
 	})
 }
