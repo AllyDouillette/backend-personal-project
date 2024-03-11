92b41c412c017b9a6296999982a2d0502f4ee214
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-11 12:35:21 +0100

    fix order of requests

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,12 +15,12 @@ import { checkToken, checkAdminRole } from "../middleware/auth.js"
 const router = Router()
 
 router.get("/", checkToken, checkAdminRole, getUsers)
-router.get("/:id", checkToken, checkAdminRole, getUser)
-router.get("/:id/cards", checkToken, checkAdminRole, getCardsFromUser)
 
 router.get("/me", checkToken, getSelf)
+router.get("/:id", checkToken, checkAdminRole, getUser)
 
 router.get("/me/cards", checkToken, getOwnCards)
+router.get("/:id/cards", checkToken, checkAdminRole, getCardsFromUser)
 
 router.get("/me/categories", checkToken, getOwnCategories)
 router.get("/me/categories/details", checkToken, getOwnCategoriesWithCards)
