8049568cd7ad13d1803f65288707df1b2fccdc16
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-03-11 12:02:42 +0100

    reorder for better readability

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,7 @@ import { getOwnCategories, getOwnCategoriesWithCards } from "../controllers/cate
 import { getOwnCards, getCardsFromUser } from "../controllers/card.js"
 import { getOwnStatistics,
 	updateOwnStatisticEntry,
+	updateOwnStatisticEntryForToday,
 	getOwnStatisticForToday,
 	getOwnStatisticForDate,
 	createOwnStatisticForDate,
@@ -14,17 +15,22 @@ import { checkToken, checkAdminRole } from "../middleware/auth.js"
 const router = Router()
 
 router.get("/", checkToken, checkAdminRole, getUsers)
-router.get("/me", checkToken, getSelf)
 router.get("/:id", checkToken, checkAdminRole, getUser)
+router.get("/:id/cards", checkToken, checkAdminRole, getCardsFromUser)
+
+router.get("/me", checkToken, getSelf)
+
 router.get("/me/cards", checkToken, getOwnCards)
+
 router.get("/me/categories", checkToken, getOwnCategories)
 router.get("/me/categories/details", checkToken, getOwnCategoriesWithCards)
+
 router.get("/me/statistics", checkToken, getOwnStatistics)
 router.patch("/me/statistics/:id", checkToken, updateOwnStatisticEntry)
 router.get("/me/statistics/today", checkToken, getOwnStatisticForToday)
+router.patch("/me/statistics/today", checkToken, updateOwnStatisticEntryForToday)
 router.get("/me/statistics/:date", checkToken, getOwnStatisticForDate)
 router.post("/me/statistics/:date", checkToken, createOwnStatisticForDate)
 router.get("/me/statistics/:startDate/:endDate", checkToken, getOwnStatisticsForDateRange)
-router.get("/:id/cards", checkToken, checkAdminRole, getCardsFromUser)
 
 export default router
